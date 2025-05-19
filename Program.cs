e6716c353605ffdc9f69c040a7416ff0aeff46f1
Marcelo <marceloamaral1364@gmail.com>
2025-05-18 22:32:31 -0300

    Finalizing the program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("11999999999", "Iphone99", "IMEIAPPLE", 128);
+iphone.InstalarAplicativo("Facebook");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+Nokia nokia = new Nokia("119888888888", "Nokia24", "IMEINOKIA", 256);
+nokia.InstalarAplicativo("Instagram");
+nokia.Ligar();
+nokia.ReceberLigacao();
